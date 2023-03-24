e0d288be36c2e29a3a854ce03365bf4e1f2e1733
Nataliia <nat-kril@yandex.ru>
2023-03-24 14:48:59 +0300

    Выполнено Задание №3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,43 @@
+'use strict';
+
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (!this.marks) {
+    return;
+  }
+
+  for (let mark of marksToAdd) {
+    this.marks.push(mark);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.marks || !this.marks.length) {
+    return 0;
+  }
+
+  let count = 0;
+  const averageMark = this.marks.reduce((acc, mark) => {
+    count++;
+    return acc + mark;
+  }, 0);
+
+  return averageMark / count;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
 }
+
